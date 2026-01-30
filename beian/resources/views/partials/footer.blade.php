4ec41e1443857148d69898ad0be5965a5c89c217
a123456 <894944672@qq.com>
2026-01-30 12:00:46 +0800

    beiancode

---
+++ beian/resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="w-full text-center text-sm text-[#706f6c] dark:text-[#A1A09A] py-4">
+    <a href="https://beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">粤ICP备2025512979号-1</a>
+</div>
--- beian/resources/views/welcome.blade.php
+++ beian/resources/views/welcome.blade.php
@@ -245,5 +245,6 @@
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
+            @include('partials.footer')
     </body>
 </html>
